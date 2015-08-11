693e2581e3f3b1737c40c640e2b8981037d06a70
furoshiki32 <kawada.hirosi@gmail.com>
2015-08-11 12:08:10 +0900

    レシピファイルを引数で指定できるように

--- printRecipe.rb
+++ printRecipe.rb
@@ -1,6 +1,15 @@
+# 引数の存在確認
+recipe_file_path = ARGV[0]
+exit unless recipe_file_path
+
 # レシピファイルから結果のフードを取得し標準出力
-File.open 'recipe-data.txt' do |recipe|
-  recipe.each_line do |result|
-    print result
+begin
+  File.open recipe_file_path do |recipe_data|
+    recipe_data.each_line do |item|
+      print item
+    end
   end
+rescue
+  # ファイルが読み込めない場合は、何も出力しない
+  exit
 end
